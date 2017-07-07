02398b50baeaea726ef8f48bf2dfa826fc37dad5
Rakesh Chotalia <Rakesh.Chotalia@uk.access-accounts.com>
2017-07-07 11:29:20 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('css', function () {
+    return gulp.src("app/styles/app.scss")
+        .pipe(sourcemaps.init())
+        .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer('last 4 version'))
+        .pipe(cssnano({
+            zindex: false
+        }))
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("public/css/"));
+});
+
+gulp.task('default', ['css'], function () {
+    gulp.watch(["app/styles/*/*.scss"], ['css']);
+});
